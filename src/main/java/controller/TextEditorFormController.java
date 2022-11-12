37469a10a81cd67755748d68a0de423dee4fe06e
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-12 22:11:25 +0530

    Close #3

--- src/main/java/controller/TextEditorFormController.java
+++ src/main/java/controller/TextEditorFormController.java
@@ -5,30 +5,39 @@ import javafx.event.ActionEvent;
 public class TextEditorFormController {
 
     public void mnuNew_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuOpen_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuSave_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuPrint_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuClose_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuCut_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuCopy_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuPaste_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuSelectAll_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuAbout_OnAction(ActionEvent actionEvent) {
